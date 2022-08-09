526d9ce8365cffbe6edacdfd8406ce7638864510
Tao Tang <tao.tang@thoughtworks.com>
2022-08-09 11:24:31 +0800

    Add core module

---
+++ modules/Core/Sources/Core.h
@@ -0,0 +1,18 @@
+//
+//  Core.h
+//  Core
+//
+//  Copyright 2022 Thoughtworks, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
