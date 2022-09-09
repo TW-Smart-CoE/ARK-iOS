abb4b647a1a8e8b5939b39760f1b39e8de2feab0
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2022-09-09 16:21:49 +0800

    Add more colors

--- modules/UI/Sources/Theme/Colors/ColorName.swift
+++ modules/UI/Sources/Theme/Colors/ColorName.swift
@@ -9,6 +9,11 @@ import Foundation
 import SwiftUI
 
 public enum ColorName: String {
+    //black
+    case black
+    //white
+    case white
+    //gray
     case gray50
     case gray100
     case gray200
@@ -19,7 +24,24 @@ public enum ColorName: String {
     case gray700
     case gray800
     case gray900
+    //blue
     case blue
+    //light blue
+    case lightBlue50
+    case lightBlue300
+    //light green
+    case lightGreen200
+    case lightGreen700
+    //purple
+    case purple200
+    case purple500
+    case purple700
+    //teal
+    case teal200
+    case teal700
+    //red
+    case red700
+    case red900
 }
 
 public extension ColorName {
