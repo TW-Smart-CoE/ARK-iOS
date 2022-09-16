6bdf1944206da4543a87bc9d44d3e0c328fc2915
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2022-09-16 14:52:32 +0800

    Fix swiftlint issues

--- modules/UI/Sources/Theme/Colors/ThemeColors.swift
+++ modules/UI/Sources/Theme/Colors/ThemeColors.swift
@@ -28,8 +28,9 @@ public extension ThemeColors {
     var color: Color {
         switch self {
         case .statusBarColor:
-            //Colors枚举相当于颜色值仓库，其他所有使用颜色值的地方都可以从这里取值。然后ThemeColors，是基于UI Guide设计的一个应用主题色集合，它可以自定义颜色值，也可以使用Colors里面定义好的色值。
-            //所以，这里的statusBarColor就是使用了Colors里面定义好的blue。
+            // Colors枚举相当于颜色值仓库，其他所有使用颜色值的地方都可以从这里取值。
+            // 然后ThemeColors，是基于UI Guide设计的一个应用主题色集合，它可以自定义颜色值，也可以使用Colors里面定义好的色值。
+            // 所以，这里的statusBarColor就是使用了Colors里面定义好的blue。
             return Colors.blue.color
         default:
             return Color(self.rawValue, bundle: .uiBundle)
