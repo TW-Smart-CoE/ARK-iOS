da1ec760cec0d8dcbbceaf47abdd3575e61553db
Jiaxin Pu <jiaxin.pu@thoughtworks.com>
2022-10-17 10:58:05 +0800

    Update Dimensions

--- modules/UI/Sources/Dimension/Dimensions.swift
+++ modules/UI/Sources/Dimension/Dimensions.swift
@@ -9,15 +9,12 @@ import Foundation
 import UIKit
 
 public extension CGFloat {
-    // spacing
-    static let standardSpacing: CGFloat = 20.0
-    static let smallSpacing: CGFloat = 4.0
-    static let halfSpacing: CGFloat = 10.0
     // dimension
     static let dimension1: CGFloat = 1.0
     static let dimension2: CGFloat = 2.0
     static let dimension4: CGFloat = 4.0
     static let dimension8: CGFloat = 8.0
+    static let dimension10: CGFloat = 10.0
     static let dimension12: CGFloat = 12.0
     static let dimension16: CGFloat = 16.0
     static let dimension20: CGFloat = 20.0
@@ -28,8 +25,12 @@ public extension CGFloat {
     static let dimension40: CGFloat = 40.0
     static let dimension44: CGFloat = 44.0
     static let dimension48: CGFloat = 48.0
+    // spacing
+    static let standardSpacing: CGFloat = dimension20
+    static let halfSpacing: CGFloat = dimension10
+    static let smallSpacing: CGFloat = dimension4
     // padding
-    static let standardPadding: CGFloat = 20.0
-    static let smallPadding: CGFloat = 4.0
-    static let halfPadding: CGFloat = 10.0
+    static let standardPadding: CGFloat = dimension16
+    static let halfPadding: CGFloat = dimension8
+    static let smallPadding: CGFloat = dimension4
 }
